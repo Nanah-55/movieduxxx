2e347671232dd161b607b43e63710711a3df2e16
Nafisa Iliyasu Hotoro <nafisailiyasuhotoro@gmail.com>
2025-12-24 12:10:39 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import '../styles.css';
+
+export default function Heedar() {
+    return(
+    <div className="header">
+        <img className="logo" src="logo.png" alt="movieduxxx" />
+        <h2 className="sub-title">It's time for popcorn! Find your next movie here.</h2>
+
+
+    </div>
+);
+}
